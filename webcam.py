14c081d0d91aae2d69ca83fce0ca2b75817df3a9
Jeff Johnston <jeff.johnston.mn@gmail.com>
2016-06-06 21:36:55 -0500

    have the video stream working

--- webcam.py
+++ webcam.py
@@ -10,6 +10,9 @@ import RPi.GPIO as GPIO
 import time
 import datetime as dt
 import subprocess
+import socket
+import threading
+from threading import Thread
 
 config = configparser.ConfigParser()
 config.read('webcam.ini')
@@ -77,14 +80,34 @@ class Webcam:
             logger.error('problem updating no-ip address: %s' % request.text)
             return False
 
+    def main(self):
+        camera = picamera.PiCamera()
+        camera.resolution = (1024, 768)
+        camera.exposure_mode = 'sports'
+        camera.vflip = True
+        camera.exposure_mode = 'auto'
+        camera.metering = 'average'
+        sleep(3)
+
+        t = threading.Timer(60.0, self.pic, args=camera)
+        t.start()
+
+    def pic(self, camera):
+        timestamp = dt.datetime.now().strftime('%m/%d %H:%M')
+        timestamp += " (" + self.get_temp() + ")"
+        camera.annotate_text = timestamp
+
+        camera.capture('/var/www/html/camera.jpg')
+
     def snapshot(self):
         camera = picamera.PiCamera()
         camera.resolution = (1024, 768)
+        camera.exposure_mode = 'sports'
         camera.vflip = True
         camera.exposure_mode = 'auto'
         camera.metering = 'average'
 
-        timestamp = dt.datetime.now().strftime('%m-%d-%Y %H:%M:%S')
+        timestamp = dt.datetime.now().strftime('%m/%d %H:%M')
         timestamp += " (" + self.get_temp() + ")"
         camera.annotate_text = timestamp
 
@@ -109,12 +132,16 @@ class Webcam:
         sleep(20)
         camera.stop_recording()
 
-    def motion(self):
+    def stream(self):
         camera = picamera.PiCamera()
         camera.resolution = (1024, 768)
-        camera.exposure_mode = 'sports'
-        sleep(2)
+        #camera.framerate = 24
+        camera.vflip = True
 
+        server_socket = ServerSocket(camera)
+        server_socket.start()
+
+    def motion(self):
         sensor = 4
         count = 0
 
@@ -133,5 +160,59 @@ class Webcam:
                 print("GPIO pin %s is %s" % (sensor, new_state))
                 if (new_state == "HIGH"):
                     count = count+1
-                    camera.capture('/home/mnrabbit/pics/jeff-%d.jpg' % count)
+                    camera = picamera.PiCamera()
+                    camera.resolution = (1024, 768)
+                    camera.exposure_mode = 'sports'
+                    camera.vflip = True
+                    camera.exposure_mode = 'auto'
+                    camera.metering = 'average'
+                    sleep(2)
+                    camera.capture('/home/mnrabbit/pics/motion-%d.jpg' % count)
+                    camera.close()
+
+class ServerSocket(Thread):
+    def __init__(self, camera):
+        Thread.__init__(self)
+        self.camera = camera
+
+    def run(self):
+        logger.debug('stream server is started')
+
+        server_socket = socket.socket()
+        server_socket.bind(('0.0.0.0', 8000))
+        server_socket.listen(0)
+
+        try:
+            while True:
+                connection = server_socket.accept()[0]
+                stream = Stream(self.camera, connection)
+                stream.start()
+        finally:
+            logger.debug('stream server is stopped')
+            server_socket.close()
+
+class Stream(Thread):
+    def __init__(self, camera, connection):
+        Thread.__init__(self)
+        self.camera = camera
+        self.connection = connection
+
+    def run(self):
+        while True:
+            try:
+                logger.debug('start the stream')
+
+                file = self.connection.makefile('wb')
+                self.camera.start_recording(file, format='h264')
+                self.camera.wait_recording(60)
+
+                logger.debug('end the stream')
+            except (BrokenPipeError, IOError, ConnectionResetError):
+                logger.debug('error caught')
+            finally:
+                self.camera.stop_recording()
+                self.connection.close()
+                file.close()
+
+
 
